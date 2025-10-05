f1af86498ea203a743fad197c516d231b7d0b43e
Ethan Hunt <ethanhunt1490@gmail.com>
2025-10-05 20:25:33 +0530

    Test - One minute row to handle 0-4 minutes - Red

--- app/src/test/java/com/srm/berlinclockdemo/domain/usecase/ConvertTimeUseCaseTest.kt
+++ app/src/test/java/com/srm/berlinclockdemo/domain/usecase/ConvertTimeUseCaseTest.kt
@@ -28,4 +28,24 @@ class ConvertTimeUseCaseTest {
         val state = useCase.execute(oddTime)
         assertEquals("O", state.secondsRow) // Off for odd seconds
     }
+
+    // Test the One-Minute Row - Red
+    @Test
+    fun testOneMinuteRow_shouldHandleZeroToFourMinutes() {
+        // 0 lights blink
+        val zeroMinuteState = useCase.execute("10:00:00")
+        assertEquals("OOOO", zeroMinuteState.oneMinutesRow)
+
+        // 1 light blink
+        val oneMinuteState = useCase.execute("10:01:00")
+        assertEquals("YOOO", oneMinuteState.oneMinutesRow)
+
+        // 4 lights blink
+        val fourMinutesState = useCase.execute("10:04:00")
+        assertEquals("YYYY", fourMinutesState.oneMinutesRow)
+
+        // 5 minutes - Remainder 0, so 0 lights blink
+        val fiveMinutesState = useCase.execute("10:05:00")
+        assertEquals("OOOO", fiveMinutesState.oneMinutesRow)
+    }
 }
