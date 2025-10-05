b36e5d3406bb329aa4b2dd98b1ba431b80e8aa95
Ethan Hunt <ethanhunt1490@gmail.com>
2025-10-06 00:54:45 +0530

    Add basic UI components for Seconds/Minutes portions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.srm.berlinclockdemo"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.viewmodel)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/srm/berlinclockdemo/presentation/BerlinClockViewModel.kt
@@ -0,0 +1,44 @@
+package com.srm.berlinclockdemo.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.srm.berlinclockdemo.data.BerlinClockState
+import com.srm.berlinclockdemo.domain.usecase.ConvertTimeUseCase
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+class BerlinClockViewModel(
+    private val convertTimeUseCase: ConvertTimeUseCase = ConvertTimeUseCase()
+) : ViewModel() {
+
+    private val _currentTimeString = MutableStateFlow(getFormattedCurrentTime())
+
+    private val _clockState = MutableStateFlow(BerlinClockState())
+    val clockState: StateFlow<BerlinClockState> = _clockState
+
+    init {
+        viewModelScope.launch {
+            while (true) {
+                val time = getFormattedCurrentTime()
+                _currentTimeString.update { time }
+                updateClock(time)
+                delay(1000L)
+            }
+        }
+    }
+
+    private fun updateClock(timeString: String) {
+        val newClockState = convertTimeUseCase.execute(timeString)
+        _clockState.update { newClockState }
+    }
+
+    private fun getFormattedCurrentTime(): String {
+        return LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+    }
+
+}
